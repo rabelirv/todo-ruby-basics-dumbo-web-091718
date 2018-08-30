454471a6a251f72c00464246fce41c37da5f3f4f
Irving E Rabel <rabel606@gmail.com>
2018-08-30 16:39:34 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,34 @@
 def division(num1, num2)
- 
+ num1/num2
 end
+division(12,2)
 
-def assign_variable(value)
-  
+def assign_variable(guy1)
+guy1="Bob"
 end
+assign_variable
 
-def argue
-  
+def argue(num=6)
+  puts num
 end
+argue
 
-def greeting
-
+def greeting(greeting="Hi", name= "Ruby Programmer")
 end
+greeting
 
 def return_a_value
-
+compliment = "Nice"
+puts compliment
 end
+return_a_value
 
 def last_evaluated_value
-
+puts "expert"
 end
+last_evaluated_value
 
-def pizza_party
-
+def pizza_party(topping="pepperoni")
+puts topping
 end
+pizza_party
